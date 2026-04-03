a248c2c7090cd51a897776576b813850b1b9c9ea
Leonid Gurianov <leonid.11521@gmail.com>
2026-04-03 17:28:47 +0300

    add docstrings

--- src/descriptors.py
+++ src/descriptors.py
@@ -3,50 +3,53 @@ from src.errors import ValidationError
 
 class IdAttribute:
     """
-
+        дескриптор для поля id
     """
-    def __set_name__(self, owner, name):
-        """
 
+    def __set_name__(self, owner, name) -> None:
+        """
+            функция для того чтобы задать имя будущему атрибуту
         """
         self.name = '_' + name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner) -> int:
         """
-
+            функция для получения атрибута
         """
         return getattr(instance, self.name, None)
 
-    def __set__(self, instance, value:int):
+    def __set__(self, instance, value: int) -> None:
         """
-
+            функция для того чтобы задать значение атрибуту id
         """
         if isinstance(value, int):
             setattr(instance, self.name, value)
         else:
             raise ValidationError
 
+
 class PriorityAttribute:
     """
-
+        дескриптор для поля Priority
     """
-    def __set_name__(self, owner, name):
-        """
 
+    def __set_name__(self, owner, name) -> None:
+        """
+            функция для того чтобы задать имя будущему атрибуту
         """
         self.name = '_' + name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner) -> int:
         """
-
+            функция для получения атрибута
         """
         return getattr(instance, self.name, None)
 
-    def __set__(self, instance, value:int):
+    def __set__(self, instance, value: int) -> None:
         """
-
+            функция для того чтобы задать значение атрибуту Priority
         """
-        if isinstance(value, int) and (value==1 or value==2):
+        if isinstance(value, int) and (value == 1 or value == 2):
             setattr(instance, self.name, value)
         else:
             raise ValidationError
@@ -54,25 +57,26 @@ class PriorityAttribute:
 
 class StatusAttribute:
     """
-
+        дескриптор для поля Status
     """
-    def __set_name__(self, owner, name):
-        """
 
+    def __set_name__(self, owner, name) -> None:
+        """
+            функция для того чтобы задать имя будущему атрибуту
         """
         self.name = '_' + name
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner) -> str:
         """
-
+            функция для получения атрибута
         """
         return getattr(instance, self.name, None)
 
-    def __set__(self, instance, value:str):
+    def __set__(self, instance, value: str) -> None:
         """
-
+            функция для того чтобы задать значение атрибуту Status
         """
-        if isinstance(value, str) and (value=='ready' or value=='in processing'):
+        if isinstance(value, str) and (value == 'ready' or value == 'in processing'):
             setattr(instance, self.name, value)
         else:
             raise ValidationError
--- src/task.py
+++ src/task.py
@@ -6,7 +6,7 @@ from src.errors import ValidationError
 
 class Task:
     """
-
+        класс задачи
     """
 
     id:int = IdAttribute()
@@ -15,7 +15,7 @@ class Task:
 
     def __init__(self, id:int, description:str,payload:dict[str, Any], priority:int):
         """
-
+            функция для инициализации атрибутов
         """
         if not isinstance(id, int):
             raise ValidationError
@@ -34,16 +34,16 @@ class Task:
         self.create_time = str(datetime.now())
 
     @property
-    def description(self):
+    def description(self) -> str:
         """
-
+            функция для получения описания задачи
         """
         return self._description
 
     @description.setter
-    def description(self, value):
+    def description(self, value) -> None:
         """
-
+            функуия для установки описания задачи
         """
         if isinstance(value, str):
             self._description = value
@@ -51,16 +51,16 @@ class Task:
             raise ValidationError
 
     @property
-    def payload(self):
+    def payload(self) -> dict:
         """
-
+            функция для получения payload
         """
         return self._payload
 
     @payload.setter
-    def payload(self, value):
+    def payload(self, value) -> None:
         """
-
+            функция для утановки payload
         """
         if isinstance(value, dict):
             self._payload = value
@@ -70,9 +70,12 @@ class Task:
     @property
     def is_ready(self) -> bool:
         """
-
+            функция для получения is_ready
         """
         return self.status=='ready'
 
     def __repr__(self):
+        """
+            функция для преобразования объекта в строку
+        """
         return "id = " + str(self.id) + ", description = " + self.description + ", payload = " + str(self.payload) + ", priority = " + str(self.priority) + ", status = " + self.status
